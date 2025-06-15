0461993fd3a0e2120dcb0fc04704dcaaf7247b30
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-06-15 23:51:42 +0600

    first commit

---
+++ firstSuccess.java
@@ -0,0 +1,7 @@
+// ফাইলের নাম হবে: firstSuccess.java
+
+public class firstSuccess{ 
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ functioncall.java
@@ -0,0 +1,10 @@
+
+public class functioncall {
+
+    public static void call(String name){
+    System.out.println("SHRE, " +name);
+}
+    public static void  main(String[] ar){
+        call("Shovon");
+    }
+}
